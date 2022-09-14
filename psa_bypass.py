695b4fd9cbf31faac3fd4234a7b3a1b9baeb4040
Romeo <92579700+JohnWickKeanue@users.noreply.github.com>
2022-09-14 13:46:27 +0530

    Update psa_bypass.py

--- psa_bypass.py
+++ psa_bypass.py
@@ -20,16 +20,16 @@ def psa_bypasser(psa_url):
 				executor.submit(try2link_scrape, exit_gate)
 			except: pass
 
-def try2link_scrape(url):
+def try2link_scrape(urlx):
 	client = cloudscraper.create_scraper(allow_brotli=False)	
 	h = {
 	'upgrade-insecure-requests': '1', 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
 	}
 	res = client.get(url, cookies={}, headers=h)
 	url = 'https://try2link.com/'+re.findall('try2link\.com\/(.*?) ', res.text)[0]
-	print(try2link_bypass(url))
+	print(try2link_bypass(urly))
 
-def try2link_bypass(url):
+def try2link_bypass(urly):
 	client = cloudscraper.create_scraper(allow_brotli=False)
 	
 	url = url[:-1] if url[-1] == '/' else url
