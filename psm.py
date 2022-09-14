59bc1f509788b136fe798c3844934b5621186855
Romeo <92579700+JohnWickKeanue@users.noreply.github.com>
2022-09-14 17:44:01 +0530

    Update psm.py

--- psm.py
+++ psm.py
@@ -12,7 +12,7 @@ def psm(url):
     h = {
           'upgrade-insecure-requests': '1',
           'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
-         }
+    }
     res = client.get(url, cookies={}, headers=h)
 
     url = 'https://try2link.com/'+re.findall('try2link\.com\/(.*?) ', res.text)[0]
@@ -28,15 +28,16 @@ def psm(url):
     bs4 = BeautifulSoup(res.content, 'html.parser')
     inputs = bs4.find_all('input')
     data = { input.get('name'): input.get('value') for input in inputs }
-
+    
     time.sleep(6.5)
     res = client.post(
-         'https://try2link.com/links/go',
-          headers={
-                   'referer': url,
-                   'x-requested-with': 'XMLHttpRequest',
-                   }, data=data
-          )
+        'https://try2link.com/links/go',
+        headers={
+            'referer': url,
+            'x-requested-with': 'XMLHttpRequest',
+        }, data=data
+    )
     out = res.json()['url'].replace('\/','/')
+    
 
 print(psm(url))
